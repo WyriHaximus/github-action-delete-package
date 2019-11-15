9e3fb18dd832f255d81a968aa72ca1ff5aeeb0e1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-11-15 08:14:30 +0100

    Initial code commit

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ $(echo ${INPUT_PACKAGEVERSIONID} | wc -c) -eq 1 ] ; then
+  echo -e "\033[31mPackage version ID cannot be empty\033[0m"
+  exit 1
+fi
+
+echo -e "{\"query\": \"mutation { deletePackageVersion(input:{packageVersionId:\\\"$INPUT_PACKAGEVERSIONID\\\"}) { success }}\"}" > /workdir/payload.json
+
+curl --request PATCH \
+  --url https://api.github.com/graphql \
+  --header "Accept: application/vnd.github.package-deletes-preview+json" \
+  --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+  --header 'Content-Type: application/json' \
+  --data "$(cat /workdir/payload.json)" \
+  -o /workdir/response.json \
+  -s
